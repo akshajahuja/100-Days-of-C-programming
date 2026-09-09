7e4491ce41ea6102836a92254673fc963bfa77b2
akshajahuja <Akshaj.41394@stu.upes.ac.in>
2026-09-09 18:54:46 +0530

    Add files via upload

---
+++ q8.c
@@ -0,0 +1,13 @@
+//Write a program to find and display the sum of the first n natural numbers.
+#include <stdio.h>
+int main(){
+    int n,i,sum=0;
+    printf("enter the first n natural numbers");
+    scanf("%d",&n);
+    for(i=1; i<=n; i++)
+    {
+        sum+=i;
+    }
+    printf("sum is %d",sum);
+    return 0;
+}

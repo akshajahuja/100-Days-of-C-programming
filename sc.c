9a00bb7dbd84fefb1ec068eec17a4dab292a14e7
akshajahuja <Akshaj.41394@stu.upes.ac.in>
2026-09-25 23:56:12 +0530

    Add files via upload

---
+++ sc.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{int n,j;
+    printf("enter n\n");
+    scanf("%d",&n);
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=(n-i); j++)
+        {
+            printf("  ");
+        }
+        for(j=1;j<=i;j++)
+        {
+            printf("* ");
+        }
+        
+    printf("\n");
+}
+}

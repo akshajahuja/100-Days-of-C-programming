b9afaecc05560aa6f2bb7b338876fc92bd24f331
akshajahuja <Akshaj.41394@stu.upes.ac.in>
2026-09-26 11:26:33 +0530

    Delete q90.c

--- q90.c
+++
@@ -1,2 +0,0 @@
-#include <stdio.h>
-int main(){char s[1000];int i;fgets(s,sizeof(s),stdin);for(i=0;s[i];i++){if(s[i]>='a'&&s[i]<='z')s[i]-=32;else if(s[i]>='A'&&s[i]<='Z')s[i]+=32;}printf("%s",s);return 0;}
